b29533efc1c21826cdc3adaf38ebb06fff17bf9a
kevin <kipronok234@gmail.com>
2024-08-05 14:54:50 +0300

    react-hooks-simple-data-fetching-lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,41 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  
+  const [dogImageUrl, setDogImageUrl] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  
+  useEffect(() => {
+    const fetchDogImage = async () => {
+      try {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random")
+        const data = await response.json();
+        
+        setDogImageUrl(data.message);
+      } catch (error) {
+        console.error("Error fetching dog image:", error);
+      } finally {
+        
+        setIsLoading(false);
+      }
+    };
+
+    
+    fetchDogImage();
+  }, [])
+
+  return (
+    <div>
+      {isLoading ? (
+        
+        <p>Loading...</p>
+      ) : (
+        
+        <img src={dogImageUrl} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
